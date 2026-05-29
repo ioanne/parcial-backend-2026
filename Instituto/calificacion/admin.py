d1a01b4cfbf27e126f15f1527614565b2e67a9b2
gogoroisler <santiago.gonzalez.roisler@gmail.com>
2026-05-29 20:13:43 -0300

    Creacion de models y admin funcionalidad retroalimentacion

--- Instituto/calificacion/admin.py
+++ Instituto/calificacion/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Retroalimentacion
+
+
+@admin.register(Retroalimentacion)
+class RetroalimentacionAdmin(admin.ModelAdmin):
+
+    # Campos visibles en la lista del admin
+    list_display = (
+        'id',
+        'asignacion',
+        'profesor',
+        'fecha_creacion'
+    )
+
+    # Permite buscar por texto del comentario
+    search_fields = (
+        'texto',
+    )
+
+    # Agrega filtro por fecha de creación
+    list_filter = (
+        'fecha_creacion',
+    )
+ 
+     # Optimiza consultas relacionadas
+    list_select_related = (
+    'asignacion',
+    'profesor',
+)
