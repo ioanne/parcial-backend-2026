6fff68d92da5c5730d6f2a42f57d577bca45ae80
Matias Ariel Castro <matias.acastro1@gmail.com>
2026-05-29 19:50:04 -0300

    crea modelo AsignacionExamen

--- Instituto/calificacion/models.py
+++ Instituto/calificacion/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class AsignacionExamen(models.Model):
+    fecha_asignacion = models.DateTimeField(auto_now_add=True)
+    fecha_resolucion = models.DateTimeField(null=True, blank=True)
+    calificacion = models.DecimalField(
+        max_digits=5, decimal_places=2, null=True, blank=True
+    )
+    observaciones = models.TextField(blank=True)
+
+    # examen = models.ForeignKey('core.Examen', on_delete=models.CASCADE)
+    # alumno = models.ForeignKey('core.Alumno', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Asignacion #{self.id} - Nota: {self.calificacion}"  # Create your models here.
